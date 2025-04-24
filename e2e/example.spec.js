623eaf91694f30b60027b98986b03a7bc35b5f09
Rocio Correa <rdpcm82@gmail.com>
2025-04-23 20:37:23 -0500

    Adicion carpetas

---
+++ e2e/example.spec.js
@@ -0,0 +1,35 @@
+import { test } from '@playwright/test';
+import fs from 'fs';
+import { PNG } from 'pngjs';
+import { options } from '../vrt.config';
+
+test.describe("color-palette", () => {
+  let beforePath = "";
+  let afterPath = "";
+  let comparePath = "";
+  let pixelmatch;
+
+  test.beforeAll(async ({ browserName }, testInfo) => {
+    pixelmatch = (await import('pixelmatch')).default;
+    beforePath = testInfo.outputPath(`before-${browserName}.png`);
+    afterPath = testInfo.outputPath(`after-${browserName}.png`);
+    comparePath = testInfo.outputPath(`compare-${browserName}.png`);
+  });
+
+  test('vrt', async ({ page }) => {
+    await page.goto("https://monitor177.github.io/color-palette/");
+    await page.screenshot({ path: beforePath });
+
+    await page.click("#generate");
+    await page.screenshot({ path: afterPath });
+
+    const img1 = PNG.sync.read(fs.readFileSync(beforePath));
+    const img2 = PNG.sync.read(fs.readFileSync(afterPath));
+
+    const { width, height } = img1;
+    const diff = new PNG({ width, height });
+
+    pixelmatch(img1.data, img2.data, diff.data, width, height, options);
+    fs.writeFileSync('diff.png', PNG.sync.write(diff));
+  });
+});
